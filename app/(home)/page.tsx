3e2754d97be45e9b7d34b79c6cd11e27b9e6a4b7
welingtonsoftware <dpepsloco@gmail.com>
2024-01-31 15:20:35 -0300

    feat: add welcome mensage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,20 @@
-import Image from "next/image";
+
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 export default function Home() {
   return (
   <div>
     <Header/>
+    <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
   </div>
   );
 }
