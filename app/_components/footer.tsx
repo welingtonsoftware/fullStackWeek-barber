237bc52a49a1e76a491b8b4ae0eeef5c2f9a8b50
welingtonsoftware <dpepsloco@gmail.com>
2024-02-02 14:39:50 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,11 @@
+import { Divide } from "lucide-react";
+
+const Footer = () => {
+  return ( 
+    <div className="w-full bg-secondary p-6 px-5" >
+      <p className="text-gray-400 text-xs font-bold"> @ 2023 Copyright FSW Barber</p>
+    </div>
+   );
+}
+ 
+export default Footer;
